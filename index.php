01dd051f82f37b32bf829550423b224de95c36ec
orlac98 <g00356556@gmit.ie>
2020-02-05 16:04:25 +0000

    ll

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <html>
     <head>
+        <link rel="stylesheet" href="styles.css">
+        
     </head>
-    <body>  
-            <h1> My website </h1>
-            <p id=demo> Hello world </p>
+    <body>
+            <h1>My Website</h1>
+            <p id=demo>Hello World!</p>
+            <?php echo '<p> Hello World PHP</p>';?>
+            <script type="text/javascript" src="myscripts.js"></script>
+            <button onclick="myFunction();">Click Me to turn text blue</button>
+            
     </body>
 </html>
